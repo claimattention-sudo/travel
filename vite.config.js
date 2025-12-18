6de2740eafd6a09ffeccce2c7c9c58988183a1ea
Maria Maestra <maria.maestra@example.com>
2025-12-18 21:50:06 +0500

    Deploy built site to gh-pages

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-
-// Конфиг Vite для GitHub Pages
-export default defineConfig({
-// root можно не указывать, по умолчанию '.'
-base: '/travel/', // ВАЖНО: имя репозитория c учётом регистра
-});
